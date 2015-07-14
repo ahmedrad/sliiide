ed7fa93a76002a525ab764e7a06a3cbb9ba3507e
Ahmed Radwan <ahmedrad@gmail.com>
2015-07-13 23:27:48 -0700

    adding the project page

---
+++ nav-icon.js
@@ -0,0 +1,15 @@
+// (function(){
+
+// 	var unclicked = true;
+// 	var navIcon = $('#nav-icon2');
+// 	navIcon.on("click",function() {
+// 		if (unclicked) {
+// 		navIcon.addClass("open");
+// 		unclicked = false;
+// 	} else {
+// 		navIcon.removeClass("open");
+// 		unclicked = true;
+// 	}
+// });
+
+// })();
